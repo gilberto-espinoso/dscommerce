ba8974634ea88722a9bae71a06dd1751e1400ef5
GilbertoSEspinoso <gilbertosilvaespinosa@gmail.com>
2023-06-27 21:22:14 -0300

    testing repository

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,15 +1,25 @@
 package com.devsuperior.dscommerce.controllers;
 
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepositoty;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepositoty repositoty;
+
     @GetMapping
     public String teste(){
-        return "Olá Mundo!";
+    Optional<Product> result = repositoty.findById(1L);
+    Product product = result.get();
+    return product.getName();
     }
 }
