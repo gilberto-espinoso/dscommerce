738f8800c757d817bb18e055c35db13a40f6201a
GilbertoSEspinoso <gilbertosilvaespinosa@gmail.com>
2023-06-29 11:01:16 -0300

    dto,CRUD, Get products paged

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,25 +1,30 @@
 package com.devsuperior.dscommerce.controllers;
 
-import com.devsuperior.dscommerce.entities.Product;
-import com.devsuperior.dscommerce.repositories.ProductRepositoty;
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
     @Autowired
-    private ProductRepositoty repositoty;
+    private ProductService service;
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        ProductDTO dto = service.findById(id);
+        return dto;
+    }
 
     @GetMapping
-    public String teste(){
-    Optional<Product> result = repositoty.findById(1L);
-    Product product = result.get();
-    return product.getName();
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
--- src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
@@ -3,5 +3,5 @@ package com.devsuperior.dscommerce.repositories;
 import com.devsuperior.dscommerce.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepositoty extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
